01111aee8a51e8cb15b3a6ffd5fb83e172bcbacd
WojciechLassak <130908780+wolas91@users.noreply.github.com>
2023-08-22 19:47:49 +0200

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import ta
-from sklearn.preprocessing import MinMaxScaler
 import mplfinance as mpf
 
 def load_data(company_name: str):
@@ -44,21 +43,17 @@ def plot_candle(df, company_name: str, interval: int, width: int,  height: int):
         figsize=(width, height)
             )
 
-# Przewidywanie ceny
-def generate_target(df, days_to_forecast = 1):
 
-    df['Perc_change'] = df.groupby('Company')['Close'].apply(lambda x: (x - x.shift(1)) / x.shift(1) * 100)
-    df.dropna(inplace=True)
+def generate_target(df, days_to_forecast=1, first_test_day='2023-01-01', last_test_day='2023-07-31'):
+    y_df = pd.DataFrame(columns=['Date', 'Company'] + [f'y_{i + 1}' for i in range(days_to_forecast)])
 
-    y_df = pd.DataFrame(columns=['Date', 'Company'] + [f'y_{i+1}' for i in range(days_to_forecast)])
-
-    for name in df['Company'].unique():
-        company = df[my_df['Company'] == name].copy()
+    for name in tqdm(df['Company'].unique(), desc="Generating Targets"):
+        company = df[df['Company'] == name].copy()
 
         y_columns = []
         for i in range(days_to_forecast):
-            company[f'y_{i+1}'] = company['Close'].shift(-i - days_to_forecast)
-            y_columns.append(f'y_{i+1}')
+            company[f'y_{i + 1}'] = company['Close'].shift(-i - days_to_forecast)
+            y_columns.append(f'y_{i + 1}')
 
         y_df = pd.concat([y_df, company[['Date', 'Company'] + y_columns]], ignore_index=True)
 
@@ -66,8 +61,30 @@ def generate_target(df, days_to_forecast = 1):
     df.reset_index(drop=True, inplace=True)
     y_df.reset_index(drop=True, inplace=True)
     X = df
-    y = y_df.iloc[:,2:].values
-    return X, y
+
+    date_features = pd.DataFrame()
+
+    for name in tqdm(X['Company'].unique(), desc="Generating Date Features"):
+        company = X[X['Company'] == name]
+        df = generate_features(company)
+        date_features = date_features.append(df, ignore_index=True)
+
+    date_features.sort_values(by=['Date', 'Company'], inplace=True)
+    date_features.reset_index(drop=True, inplace=True)
+    nan_indices = date_features[date_features.isna().any(axis=1)].index
+    X_cleaned = date_features.drop(nan_indices)
+    y_cleaned = y_df.drop(nan_indices)
+    X_cleaned.reset_index(drop=True, inplace=True)
+    y_cleaned.reset_index(drop=True, inplace=True)
+
+    train_condition = X_cleaned['Date'] < first_test_day
+    X_train, y_train = X_cleaned[train_condition], y_cleaned[train_condition]
+
+    test_condition = (X_cleaned['Date'] >= first_test_day) & (X_cleaned['Date'] <= last_test_day)
+    X_test, y_test = X_cleaned[test_condition], y_cleaned[test_condition]
+
+    y_train, y_test = y_train.iloc[:, 2:].values, y_test.iloc[:, 2:].values
+    return X_train, y_train, X_test, y_test
 
 def calculate_mape(actual, pred):
     actual, pred = np.array(actual), np.array(pred)
@@ -83,52 +100,52 @@ def generate_features(date):
     df['High'] = date['High']
     df['Low'] = date['Low']
     df['Volume'] = date['Volume']
-    # Średnie ceny
+
     df['Close_diff'] = date['Close'] - date['Close'].shift(1)
     df['Close_diff_perc'] = (date['Close'] - date['Close'].shift(1)) / date['Close'].shift(1) * 100
     df['Open_diff'] = date['Open'] - date['Open'].shift(1)
     df['Open_diff_perc'] = (date['Open'] - date['Open'].shift(1)) / date['Open'].shift(1) * 100
     # Średnie wolumeny
-    df['avg_volume_5'] = df['Volume'].rolling(5).mean()
-    df['avg_volume_10'] = df['Volume'].rolling(10).mean()
-    df['avg_volume_30'] = df['Volume'].rolling(21).mean()
-    df['ratio_avg_volume_5_30'] = df['avg_volume_5'] / df['avg_volume_30']
-    # Odchylenia standardowe cen
-    df['std_price_5'] = df['Close'].rolling(5).std()
-    df['std_price_30'] = df['Close'].rolling(21).std()
-    df['ratio_std_price_5_30'] = df['std_price_5'] / df['std_price_30']
-    # Odchylenia standardowe wolumenów
-    df['std_volume_5'] = df['Volume'].rolling(5).std()
-    df['std_volume_30'] = df['Volume'].rolling(21).std()
-    df['ratio_std_volume_5_30'] = df['std_volume_5'] / df['std_volume_30']
+    df['Volume_1w'] = df['Volume'].rolling(5).mean()
+    df['Volume_2w'] = df['Volume'].rolling(10).mean()
+    df['Volume_1m'] = df['Volume'].rolling(21).mean()
+#     df['Ratio_volume_1w_2w'] = df['Volume_1w'] / df['Volume_2w']
+    df['Ratio_volume_1w_1m'] = df['Volume_1w'] / df['Volume_1m']
+    # Odchylenia standardowe
+    df['Std_price_1w'] = df['Close'].rolling(5).std()
+    df['Std_price_1m'] = df['Close'].rolling(21).std()
+    df['Ratio_std_price_1w_1m'] = df['Std_price_1w'] / df['Std_price_1m']
+#     df['Std_volume_1w'] = df['Volume'].rolling(5).std()
+#     df['Std_volume_1m'] = df['Volume'].rolling(21).std()
+#     df['Ratio_std_volume_1w_1m'] = df['Std_volume_1w'] / df['Std_volume_1m']
     # Zwroty
-    df['Return_1'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
-    df['Return_5'] = ((df['Close'] - df['Close'].shift(5)) / df['Close'].shift(5)).shift(1)
-    df['Return_30'] = ((df['Close'] - df['Close'].shift(21)) / df['Close'].shift(21)).shift(1)
-    df['moving_avg_5'] = df['return_1'].rolling(5).mean().shift(1)
-    df['moving_avg_30'] = df['return_1'].rolling(21).mean().shift(1)
+    df['Return_1d'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
+    df['Return_1w'] = ((df['Close'] - df['Close'].shift(5)) / df['Close'].shift(5)).shift(1)
+    df['Return_1m'] = ((df['Close'] - df['Close'].shift(21)) / df['Close'].shift(21)).shift(1)
+#     df['Moving_avg_1w'] = df['Return_1d'].rolling(5).mean().shift(1)
+    df['Moving_avg_1m'] = df['Return_1d'].rolling(21).mean().shift(1)
     # Wskaźniki
     df['SMA_5'] = df['Close'].rolling(5).mean()
     df['SMA_10'] = df['Close'].rolling(10).mean()
     df['SMA_30'] = df['Close'].rolling(21).mean()
-    df['EMA_10'] = ta.trend.EMAIndicator(df['Close'], window: int = 10).ema_indicator()
-    df['EMA_20'] = ta.trend.EMAIndicator(df['Close'], window: int = 20).ema_indicator()
+    df['EMA_10'] = ta.trend.EMAIndicator(df['Close'], 10).ema_indicator()
+    df['EMA_20'] = ta.trend.EMAIndicator(df['Close'], 20).ema_indicator()
     df['MACD'] = ta.trend.MACD(df['Close']).macd()
     df['MACD_diff'] = ta.trend.MACD(df['Close']).macd_diff()
     df['MACD_signal'] = ta.trend.MACD(df['Close']).macd_signal()
     df['BOLL_high']= ta.volatility.BollingerBands(df['Close']).bollinger_hband()
     df['BOLL_low']= ta.volatility.BollingerBands(df['Close']).bollinger_lband()
-    df['BOLL_perc']= ta.volatility.BollingerBands(df['Close']).bollinger_pband()
-    df['ROC_10'] = ta.momentum.ROCIndicator(df['Close'], window: int = 10).roc()
-    df['ROC_20'] = ta.momentum.ROCIndicator(df['Close'], window: int = 20).roc()
-    df['MI'] = ta.trend.MassIndex(df['High'], df['Low']).mass_index()
+#     df['BOLL_perc']= ta.volatility.BollingerBands(df['Close']).bollinger_pband()
+    df['ROC_10'] = ta.momentum.ROCIndicator(df['Close'], 10).roc()
+    df['ROC_20'] = ta.momentum.ROCIndicator(df['Close'], 20).roc()
+#     df['MI'] = ta.trend.MassIndex(df['High'], df['Low']).mass_index()
     df['OBV'] = ta.volume.OnBalanceVolumeIndicator(df['Close'], df['Volume']).on_balance_volume()
     df['NVI'] = ta.volume.NegativeVolumeIndexIndicator(df['Close'], df['Volume']).negative_volume_index()
     df['ADX'] = ta.trend.ADXIndicator(df['High'], df['Low'], df['Close']).adx()
-    df['CMF'] = ta.volume.ChaikinMoneyFlowIndicator(df['High'], df['Low'], df['Close'], df['Volume']).chaikin_money_flow()
-    df['PVO'] = ta.momentum.pvo(df['Volume'])
-    df['PVO_hist'] = ta.momentum.pvo_hist(df['Volume'])
-    df['PVO_signal'] = ta.momentum.pvo_signal(df['Volume'])
-    df['UO'] = ta.momentum.UltimateOscillator(df['High'], df['Low'], df['Close']).ultimate_oscillator()
-    df['DPO'] = ta.trend.DPOIndicator(df['Close']).dpo() 
+#     df['CMF'] = ta.volume.ChaikinMoneyFlowIndicator(df['High'], df['Low'], df['Close'], df['Volume']).chaikin_money_flow()
+#     df['PVO'] = ta.momentum.pvo(df['Volume'])
+#     df['PVO_hist'] = ta.momentum.pvo_hist(df['Volume'])
+#     df['PVO_signal'] = ta.momentum.pvo_signal(df['Volume'])
+#     df['UO'] = ta.momentum.UltimateOscillator(df['High'], df['Low'], df['Close']).ultimate_oscillator()
+#     df['DPO'] = ta.trend.DPOIndicator(df['Close']).dpo()
     return df
